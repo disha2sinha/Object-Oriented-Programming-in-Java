67d9ce5f3c6a312c396d33ddcc6a5a10bfd9c7ef
Disha Sinha <42354197+disha2sinha@users.noreply.github.com>
2019-10-28 13:05:03 +0530

    Add files via upload

---
+++ this keyword/this2.java
@@ -0,0 +1,25 @@
+
+// Java code for using this() to  
+// invoke current class constructor 
+class Test {
+    int a;
+    int b;
+
+    // Default constructor
+    Test() {
+        this(10, 20);
+        System.out.println("Inside  default constructor \n");
+    }
+
+    // Parameterized constructor
+    Test(int a, int b) {
+        this.a = a;
+        this.b = b;
+        System.out.println("Inside parameterized constructor");
+    }
+}
+class this2{
+    public static void main(String[] args) {
+        Test object = new Test();
+    }
+}
