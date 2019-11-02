e3c5aa4d129d406bc467fb7820086eec7056e9f0
Disha Sinha <42354197+disha2sinha@users.noreply.github.com>
2019-11-02 20:43:42 +0530

    Add files via upload

---
+++ Constructors/PrivateConstructor.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+class Person {
+    static Person name = null;
+    public int age = 12;
+
+    // private constructor can't be accessed outside the class
+    private Person() {
+    }
+
+    // Factory method to provide the users with instances
+    static public Person getInstance() {
+        if (name == null)
+            name = new Person();
+
+        return name;
+    }
+}
+
+class PrivateConstructor{
+    public static void main(String args[]) {
+        Person a = Person.getInstance();
+        Person b = Person.getInstance();
+        a.age = a.age + 10;
+        System.out.println("Value of a.age = " + a.age);
+        System.out.println("Value of b.age = " + b.age);
+        // We changed value of a.age, value of b.age also got updated because both ‘a’
+        // and ‘b’ refer to same object, i.e., they are objects of a singleton class.
+    }
+}
