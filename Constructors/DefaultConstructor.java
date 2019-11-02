f6b3002a5decafec6666670f213203d10eed317b
Disha Sinha <42354197+disha2sinha@users.noreply.github.com>
2019-11-02 11:06:21 +0530

    Add files via upload

---
+++ Constructors/DefaultConstructor.java
@@ -0,0 +1,28 @@
+class A
+{
+    int i;
+    float f;
+    boolean b;
+    double d;
+    long l;
+    char c;
+    String s;
+    short st;
+    byte bt;
+}
+class DefaultConstructor {
+    public static void main(String[] args) {
+        A obj=new A();
+        System.out.println(obj.i);
+        System.out.println(obj.f);
+        System.out.println(obj.b);
+        System.out.println(obj.d);
+        System.out.println(obj.l);
+        System.out.println(obj.c);
+        System.out.println(obj.s);
+        System.out.println(obj.st);
+        System.out.println(obj.bt);
+
+    }
+    
+}
