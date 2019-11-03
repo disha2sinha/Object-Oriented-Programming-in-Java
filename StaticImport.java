c176931fcd9a52810e9c6f0a0020057bf4ef9fb4
Disha Sinha <42354197+disha2sinha@users.noreply.github.com>
2019-11-03 13:41:39 +0530

    Add files via upload

---
+++ StaticImport.java
@@ -0,0 +1,13 @@
+import static java.lang.System.*;
+
+/**
+ * StaticImport
+ */
+class StaticImport {
+    public static void main(String[] args) {
+        // We don't need to use 'System.out'
+        // as imported using static.
+        out.println("Static Import");
+    }
+    
+}
