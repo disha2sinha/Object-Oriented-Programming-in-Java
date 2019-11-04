ac5de679327ef78fd0dc5674083c63542b843332
Disha Sinha <42354197+disha2sinha@users.noreply.github.com>
2019-11-04 13:00:48 +0530

    Add files via upload

---
+++ Interface/NestedInterface1.java
@@ -0,0 +1,26 @@
+package Interface;
+class MyClass
+{
+    interface NestedInterface {
+        String student="Disha";//needs to be initia;ized as its final
+        void display(int rollno);
+    }
+}
+class MyIface implements MyClass.NestedInterface
+{
+    public void display(int rno)
+    {
+        System.out.println("Rollno:"+rno);
+    }
+}
+/**
+ * NestedInterface1
+ */
+class NestedInterface1 {
+    public static void main(String[] args) {
+        MyClass.NestedInterface obj=new MyIface();
+        System.out.println("Name:"+MyClass.NestedInterface.student);
+        obj.display(75);
+    }
+    
+}
